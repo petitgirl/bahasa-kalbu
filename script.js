45e4b4d6c81897e7850f2d3392a43d41423f3463
petitgirl <jamujago327@gmail.com>
2024-07-28 12:54:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+const encryptionMap = {
+  'A': '@', 'a': '@',
+  'E': '3', 'e': '3',
+  'I': '1', 'i': '1',
+  'O': '0', 'o': '0',
+  'U': '|_|', 'u': '|_|',
+  'B': '|3', 'b': '|3',
+  'C': '(', 'c': '(',
+  'D': '|)', 'd': '|)',
+  'F': '|=', 'f': '|=',
+  'G': '6', 'g': '6',
+  'H': '|-|', 'h': '|-|',
+  'J': ']', 'j': ']',
+  'K': '|<', 'k': '|<',
+  'L': '|_', 'l': '|_',
+  'M': '|\\/|', 'm': '|\\/|',
+  'N': '|\\|', 'n': '|\\|',
+  'P': '|*', 'p': '|*',
+  'Q': '(u', 'q': '(u',
+  'R': '|2', 'r': '|2',
+  'S': '5', 's': '5',
+  'T': '+', 't': '+',
+  'V': '\\/', 'v': '\\/',
+  'W': '\\/\\/', 'w': '\\/\\/',
+  'X': '><', 'x': '><',
+  'Y': '`/', 'y': '`/',
+  'Z': '2', 'z': '2'
+};
+
+const decryptionMap = Object.fromEntries(
+  Object.entries(encryptionMap).map(([key, value]) => [value, key])
+);
+
+function encrypt(text) {
+  return text.split('').map(char => encryptionMap[char] || char).join('');
+}
+
+function decrypt(text) {
+  let decryptedText = '';
+  let i = 0;
+
+  while (i < text.length) {
+    let found = false;
+    for (const [encChar, decChar] of Object.entries(decryptionMap)) {
+      if (text.slice(i, i + encChar.length) === encChar) {
+        decryptedText += decChar;
+        i += encChar.length;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      decryptedText += text[i];
+      i++;
+    }
+  }
+
+  return decryptedText;
+}
+
+function encryptText() {
+  const inputText = document.getElementById('inputText').value;
+  const encryptedText = encrypt(inputText);
+  document.getElementById('outputText').value = encryptedText;
+}
+
+function decryptText() {
+  const inputText = document.getElementById('inputText').value;
+  const decryptedText = decrypt(inputText);
+  document.getElementById('outputText').value = decryptedText;
+}

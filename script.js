200725455c75b8c9b3aa48bf6c6376ee2280cb34
petitgirl <jamujago327@gmail.com>
2024-07-28 13:21:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
         const substitution = {
-            'A': '4', 'B': '8', 'C': '(', 'D': '|)', 'E': '3', 'F': '|=', 'G': '6', 'H': '#', 'I': '1', 
-            'J': '_|', 'K': '|<', 'L': '1_', 'M': '/\\/\\', 'N': '/\\/', 'O': '0', 'P': '|D', 'Q': '(,)', 
-            'R': '|2', 'S': '5', 'T': '+', 'U': '|_', 'V': '\\/', 'W': '\\/\\/', 'X': '><', 'Y': '`/', 'Z': '2',
-            'a': '4', 'b': '8', 'c': '(', 'd': '|)', 'e': '3', 'f': '|=', 'g': '6', 'h': '#', 'i': '1', 
-            'j': '_|', 'k': '|<', 'l': '1_', 'm': '/\\/\\', 'n': '/\\/', 'o': '0', 'p': '|D', 'q': '(,)', 
-            'r': '|2', 's': '5', 't': '+', 'u': '|_', 'v': '\\/', 'w': '\\/\\/', 'x': '><', 'y': '`/', 'z': '2'
+            'A': '@', 'B': '8', 'C': '(', 'D': 'd', 'E': '€', 'F': '|=', 'G': '9', 'H': 'h', 'I': '!', 
+            'J': 'j', 'K': '|<', 'L': 'L', 'M': 'm', 'N': '|\\|', 'O': '0', 'P': 'p', 'Q': 'q', 
+            'R': 'r', 'S': '5', 'T': '7', 'U': 'u', 'V': '\\/', 'W': '\\/\\/', 'X': '><', 'Y': 'y', 'Z': '2',
+            'a': '@', 'b': 'b', 'c': '(', 'd': 'd', 'e': '€', 'f': '|=', 'g': '9', 'h': 'h', 'i': '!', 
+            'j': 'j', 'k': '|<', 'l': 'L', 'm': 'm', 'n': '|\\|', 'o': '0', 'p': 'p', 'q': 'q', 
+            'r': 'r', 's': '5', 't': '7', 'u': 'u', 'v': '\\/', 'w': '\\/\\/', 'x': '><', 'y': 'y', 'z': '2'
         };
 
         const reverseSubstitution = Object.fromEntries(Object.entries(substitution).map(([k, v]) => [v, k]));
@@ -21,19 +21,20 @@
         function decryptText() {
             const inputText = document.getElementById('inputText').value;
             let decryptedText = '';
-            let skip = 0;
-            for (let i = 0; i < inputText.length; i++) {
-                if (skip > 0) {
-                    skip--;
-                    continue;
+            let i = 0;
+            while (i < inputText.length) {
+                let char = inputText[i];
+                if (char === '|') {
+                    const nextChar = inputText[i + 1];
+                    const combinedChar = char + nextChar;
+                    if (reverseSubstitution[combinedChar]) {
+                        decryptedText += reverseSubstitution[combinedChar];
+                        i += 2;
+                        continue;
+                    }
                 }
-                const twoChar = inputText.substring(i, i + 2);
-                if (reverseSubstitution[twoChar]) {
-                    decryptedText += reverseSubstitution[twoChar];
-                    skip = 1;
-                } else {
-                    decryptedText += reverseSubstitution[inputText[i]] || inputText[i];
-                }
+                decryptedText += reverseSubstitution[char] || char;
+                i++;
             }
             document.getElementById('outputText').value = decryptedText;
         }
